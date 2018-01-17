d18b442fc40850f2c115ea9551c8b0d75add2ea2
Maxime Golfier <maxgolfier@gmail.com>
2018-01-17 10:31:03 +0100

    iii

--- app.js
+++ app.js
@@ -10,6 +10,36 @@ var users = require('./routes/users');
 
 var app = express();
 
+
+var cfenv = require("cfenv");
+
+
+/***** IBM part *****/
+var vcapLocal; // load local VCAP configuration  and service credentials
+try {
+  vcapLocal = require('./vcap-local.json');
+  //console.log("Loaded local VCAP", vcapLocal);
+} catch (e) { }
+
+const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {}
+
+const appEnv = cfenv.getAppEnv(appEnvOpts);
+
+if (appEnv.services['conversation']) {
+  // Load the Watson Conversation library.
+  var ConversationV1 = require('watson-developer-cloud/conversation/v1');
+
+  // Initialize database with credentials
+  conversation = new ConversationV1({
+    username: appEnv.services['conversation'][0].credentials.username,
+    password: appEnv.services['conversation'][0].credentials.password,
+    version_date: ConversationV1.VERSION_DATE_2017_05_26
+  });
+  //console.log(conversation);
+}
+
+/***** IBM part *****/
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
