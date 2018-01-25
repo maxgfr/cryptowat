6ed9c6f723578e5564813a0c17e2244add191d32
Maxime Golfier <maxgolfier@gmail.com>
2018-01-25 15:03:09 +0100

    ii

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var coinmarketcap = require("coinmarketcap");
+global.fetch = require('node-fetch');
+
+let context;
+let value;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,20 +17,51 @@ router.get('/', function(req, res, next) {
     }
 });
 
-router.post('/', function(req, res, next) {
+router.post('/',async function(req, res, next) {
     //console.log(req.body.input);
     conversation.message({
         input: { text: req.body.input},
-        workspace_id: 'd2fc90d8-c914-4705-bbc7-1b5175ab4066'
+        context: context,
+        workspace_id: '3ca0aa0a-f518-4e60-b233-38056953fa71'
     }, function(err, response) {
         if (err) {
            console.error(err);
        } else {
-           //console.log(response.output.text);
-           res.send(response.output.text);
+           console.log(response);
+           callPrice(response);
        }
-   });
+    });
+    async function callPrice(response) {
+        var rep = response.output.text;
+        context = response.context;
+        if (context.currency != null && context.period != null) {
+            var data =  await coinmarketcap.tickerByAsset(context.currency);
+            var name = data.name;
+            var marketcap = data.market_cap_usd;
+            var price_usd = data.price_usd;
+            var period = 'none';
+            console.log(data);
+            if(context.period == "weekly") {
+                value = data.percent_change_7d;
+                period = '7 days';
+            }
+            if (context.period == "daily") {
+                value = data.percent_change_24h;
+                period = '24 hours';
+            }
+            if (context.period == "hourly"){
+                value = data.percent_change_1h;
+                period = 'hour';
+            }
+            var result = 'The marketcap of '+name+' is '+marketcap+ '$. He has got a price of '+price_usd+'$. The last '+period+', the price has change of '+value+'%.';
+            console.log(result);
+            context = null;
+            res.send([result]);
+        }
+        res.send([rep]);
+    }
 });
+
 
 router.post('/wabhook', function(req, res, next) {
 
