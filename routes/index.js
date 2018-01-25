fc3934d6e96350768b64ea31abc1ef52b0320411
Maxime Golfier <maxgolfier@gmail.com>
2018-01-25 19:58:49 +0100

    prepare for facebook

--- routes/index.js
+++ routes/index.js
@@ -53,7 +53,12 @@ router.post('/',async function(req, res, next) {
                 value = data.percent_change_1h;
                 period = 'hour';
             }
-            var result = 'The marketcap of '+name+' is '+marketcap+ '$. He has got a price of '+price_usd+'$. The last '+period+', the price has change of '+value+'%.';
+            var result;
+            if(Math.sign(value) == 1) { //positive
+                result = 'The last '+period+', the price of '+name+' increased by '+value+'%. Now, its price is: '+price_usd+'$.';
+            } else {
+                result = 'The last '+period+', the price of '+name+' fell to '+value+'%. Now, its price is: '+price_usd+'$.';
+            }
             console.log(result);
             context = null;
             res.send([result]);
@@ -62,9 +67,54 @@ router.post('/',async function(req, res, next) {
     }
 });
 
-
 router.post('/wabhook', function(req, res, next) {
-
+    //console.log(req.body.input);
+    conversation.message({
+        input: { text: req.body.input},
+        context: context,
+        workspace_id: '3ca0aa0a-f518-4e60-b233-38056953fa71'
+    }, function(err, response) {
+        if (err) {
+           console.error(err);
+       } else {
+           console.log(response);
+           callPrice(response);
+       }
+    });
+    async function callPrice(response) {
+        var rep = response.output.text;
+        context = response.context;
+        if (context.currency != null && context.period != null) {
+            var data =  await coinmarketcap.tickerByAsset(context.currency);
+            var name = data.name;
+            var marketcap = data.market_cap_usd;
+            var price_usd = data.price_usd;
+            var period = 'none';
+            console.log(data);
+            if(context.period == "weekly") {
+                value = data.percent_change_7d;
+                period = '7 days';
+            }
+            if (context.period == "daily") {
+                value = data.percent_change_24h;
+                period = '24 hours';
+            }
+            if (context.period == "hourly"){
+                value = data.percent_change_1h;
+                period = 'hour';
+            }
+            var result;
+            if(Math.sign(value) == 1) { //positive
+                result = 'The last '+period+', the price of '+name+' increased by '+value+'%. Now, its price is: '+price_usd+'$.';
+            } else {
+                result = 'The last '+period+', the price of '+name+' fell to '+value+'%. Now, its price is: '+price_usd+'$.';
+            }
+            console.log(result);
+            context = null;
+            res.send([result]);
+        }
+        res.send([rep]);
+    }
 });
 
 module.exports = router;
